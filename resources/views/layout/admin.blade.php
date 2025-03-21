6f9bcdc93dcee382d3e53df3e6d89211f3c74e3e
nicecarmel <euniceayigbede@gmail.com>
2025-03-21 15:58:39 +0100

    Initialisation du projet

---
+++ resources/views/layout/admin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset('admin/bootstrap.min.css')}}">
+     @yield('link')
+</head>
+<body>
+    <header>
+        @include('admin._composants.header')
+    </header>
+
+   <main> @yield('content')</main>
+
+  <div class="container">
+    <footer>
+
+    @include('admin._composants.footer')
+    </footer>
+  </div>
+  <script src=" {{ asset('admin/bootstrap.bundle.min.js') }} "></script>
+  @yield('script')
+</body>
+</html>
