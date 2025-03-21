858347f435a76b241056922fab163f6927c12c36
nicecarmel <euniceayigbede@gmail.com>
2025-03-21 16:17:07 +0100

    Ajout des modeles et migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/home',[HomeController::class,'admin'])->name('admin.acceuil');
+
